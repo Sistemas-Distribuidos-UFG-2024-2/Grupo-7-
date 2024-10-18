6cd800674c4e3082cdd3833b02d1d6bc7ff1919d
MatheusYosimura <matheuslimayosimura@outlook.com>
2024-10-18 18:55:57 -0300

    Problemas de Programação - 1

--- Java/sistemas-distribuidos/src/atividade1/ServerFinder.java
+++ Java/sistemas-distribuidos/src/atividade1/ServerFinder.java
@@ -4,18 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServerFinder {
-	private static List<Integer> portas = new ArrayList<>();	
-	public ServerFinder() {
-	}
-
-	public static void lancaPorta(int porta) {
-		portas.add(Integer.valueOf(porta));
-		System.out.println("Servidor localhost:"+porta);
-	}
-	public static List<Integer> retornaPortas(){
-		for(int i : portas) {
-			System.out.println(i);
-		}
-		return  portas;
+	public static void main(String[] args) {
+		int a = 2;
+		System.out.println("3"+a);
 	}
 }
---
+++ Java/sistemas-distribuidos/src/atividade1/asdasd.java
@@ -0,0 +1,21 @@
+package atividade1;
+
+public class asdasd {
+	public static void main(String[] args) {
+		int a;
+		System.out.println("1");
+		a=1;
+		System.out.println("2");
+		a=2;
+		System.out.println("3");
+		a=3;
+		System.out.println("4");
+		a=4;
+		System.out.println("5");
+		a=5;
+		System.out.println("6");
+		a=6;
+		System.out.println("7");
+		a=7;
+	}
+}
